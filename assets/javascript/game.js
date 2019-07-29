f2c23c1e3c548d8cce54af22004d47069a5a1ccf
Arman Bareyan <abareyan@gmail.com>
2019-07-29 16:37:08 -0600

    Update all files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,32 @@
-var start = true;
-var leiaFirst = false;
-var chewbaccaFirst = false;
-var reyFirst = false;
-var darthFirst = false;
-var leiaSecond = false;
-var chewbaccaSecond = false;
-var reySecond = false;
-var darthSecond = false;
-var secondChoise = false;
-var leiaScore = 120;
-var chewbaccaScore = 140;
-var reyScore = 90;
-var dartScore = 180;
-var yourcount = 0;
-var enemyScore = 0;
+var start = true; // for the choice character from enemy section
+var leiaFirst = false; // if user choice is Leia
+var leiaSecond = false; // if user choice enemy is Leia
+var chewbaccaFirst = false; // if user choice is Chewbacca
+var chewbaccaSecond = false; // if user choice enemy is Chewbacca
+var reyFirst = false; // if user choice is Rey
+var reySecond = false; // if user choice enemy is Rey
+var darthFirst = false; // if user choice is Darth
+var darthSecond = false; // if user choice enemy is Rey
+var userCount = 0; // for user health points
+var userAttackPower = 0; // for user attack power
+var userName; // for user full name
+var enemyScore = 0; // for enemy health points
+var enemyAttackPower = 0; // for enemy attack power
+var enemyName; // for enemy full name
+var count = 0; // if character win
+var userUpper; // upper case of character full name
+var winnerImage; // image of character if win
 
-var sound = document.getElementById("star");
+var sound = document.getElementById("star"); // sound
 
+// object with characters
 var leia = {
     name: 'leia',
     image: '<img src="assets/images/leya.jpg" alt="Leia">',
     fullName: 'Princess Leia',
     healthPoints: 120,
-    attackPower: 13,
-    counterAttackPower: 13
+    attackPower: 25,
+    counterAttackPower: 25
 }
 
 var chubaka = {
@@ -31,17 +34,17 @@ var chubaka = {
     image: '<img src="assets/images/chubaka.jpg" alt="Chewbacca">',
     fullName: 'Chewbacca',
     healthPoints: 140,
-    attackPower: 19,
-    counterAttackPower: 19
+    attackPower: 20,
+    counterAttackPower: 20
 }
 
 var rey = {
     name: 'rey',
     image: '<img src="assets/images/rey.png" alt="Rey">',
     fullName: 'Rey',
-    healthPoints: 90,
-    attackPower: 23,
-    counterAttackPower: 23
+    healthPoints: 160,
+    attackPower: 15,
+    counterAttackPower: 15
 }
 
 var dart = {
@@ -52,7 +55,6 @@ var dart = {
     attackPower: 10,
     counterAttackPower: 10
 }
-
 
 $(document).ready(function() {
 
@@ -88,21 +90,26 @@ $(document).ready(function() {
             });
     };
 
-
+    // for choosing character
     $('.push').click(function() {
 
-        erase();
+        erase(); // erase all characters from characters list
+
         $('#enemy').animate({
                 opacity: '1'
             }, "slow", 'swing',
             function() {
                 $('#enemy').html("<h2>Enemies available to attack</h2>");
             });
-
+        // choosing character for fight
         if ($(this).attr("id") === "left") {
             leiaFirst = true;
             leiaSecond = true;
-            yourcount = leia.healthPoints;
+            userCount = leia.healthPoints;
+            userAttackPower = leia.attackPower;
+            userName = leia.fullName;
+            winnerImage = leia.image;
+
             $('.bottomLeft').animate({
                     opacity: '1'
                 },
@@ -110,14 +117,16 @@ $(document).ready(function() {
                 function() {
                     $('#nameL').html(leia.fullName);
                     $('#newL').html(leia.image);
-                    $('#scoreL').text(yourcount);
-
-
+                    $('#scoreL').text(userCount);
                 });
         } else if ($(this).attr("id") === "leftMiddle") {
             chewbaccaFirst = true;
             chewbaccaSecond = true;
-            yourcount = chubaka.healthPoints;
+            userCount = chubaka.healthPoints;
+            userAttackPower = chubaka.attackPower;
+            userName = chubaka.fullName;
+            winnerImage = chubaka.image;
+
             $('.bottomLeft').animate({
                     opacity: '1'
                 },
@@ -125,13 +134,16 @@ $(document).ready(function() {
                 function() {
                     $('#nameL').html(chubaka.fullName);
                     $('#newL').html(chubaka.image);
-                    $('#scoreL').text(yourcount);
-
+                    $('#scoreL').text(userCount);
                 });
         } else if ($(this).attr("id") === "rightMiddle") {
             reyFirst = true;
             reySecond = true;
-            yourcount = rey.healthPoints;
+            userCount = rey.healthPoints;
+            userAttackPower = rey.attackPower;
+            userName = rey.fullName;
+            winnerImage = rey.image;
+
             $('.bottomLeft').animate({
                     opacity: '1'
                 },
@@ -139,12 +151,16 @@ $(document).ready(function() {
                 function() {
                     $('#nameL').html(rey.fullName);
                     $('#newL').html(rey.image);
-                    $('#scoreL').text(yourcount);
+                    $('#scoreL').text(userCount);
                 });
         } else {
             darthFirst = true;
             darthSecond = true;
-            yourcount = dart.healthPoints;
+            userCount = dart.healthPoints;
+            userAttackPower = dart.attackPower;
+            userName = dart.fullName;
+            winnerImage = dart.image;
+
             $('.bottomLeft').animate({
                     opacity: '1'
                 },
@@ -152,9 +168,11 @@ $(document).ready(function() {
                 function() {
                     $('#nameL').html(dart.fullName);
                     $('#newL').html(dart.image);
-                    $('#scoreL').text(yourcount);
+                    $('#scoreL').text(userCount);
                 });
         }
+
+        // change enemies place
 
         if (leiaFirst) {
             $('.top2').animate({
@@ -236,10 +254,20 @@ $(document).ready(function() {
                 });
             darthFirst = false;
         }
-
     });
 
+
+    // choosing character for fight
     $('.pushSecond').click(function() {
+        $('#comment').animate({
+                opacity: '1'
+            },
+            "slow", "swing",
+            function() {
+                $('#comment').text("");
+            });
+
+
         if (start) {
             $('#vs').animate({
                     opacity: '1'
@@ -250,8 +278,6 @@ $(document).ready(function() {
                 });
 
             if ($(this).attr("id") === "topLeft") {
-                console.log(this);
-
                 $('#topLeft').animate({
                         opacity: '0.1'
                     },
@@ -262,6 +288,9 @@ $(document).ready(function() {
 
                 if (leiaSecond) {
                     enemyScore = chubaka.healthPoints;
+                    enemyAttackPower = chubaka.counterAttackPower;
+                    enemyName = chubaka.fullName;
+
                     $('.bottomRight').animate({
                             opacity: '1'
                         },
@@ -273,6 +302,9 @@ $(document).ready(function() {
                         });
                 } else {
                     enemyScore = leia.healthPoints;
+                    enemyAttackPower = leia.counterAttackPower;
+                    enemyName = leia.fullName;
+
                     $('.bottomRight').animate({
                             opacity: '1'
                         },
@@ -283,7 +315,6 @@ $(document).ready(function() {
                             $('#scoreR').text(enemyScore);
                         });
                 }
-
 
             } else if ($(this).attr("id") === "topMiddle") {
                 console.log(this);
@@ -297,6 +328,9 @@ $(document).ready(function() {
 
                 if (leiaSecond || chewbaccaSecond) {
                     enemyScore = rey.healthPoints;
+                    enemyAttackPower = rey.counterAttackPower;
+                    enemyName = rey.fullName;
+
                     $('.bottomRight').animate({
                             opacity: '1'
                         },
@@ -308,6 +342,9 @@ $(document).ready(function() {
                         });
                 } else {
                     enemyScore = chubaka.healthPoints;
+                    enemyAttackPower = chubaka.counterAttackPower;
+                    enemyName = chubaka.fullName;
+
                     $('.bottomRight').animate({
                             opacity: '1'
                         },
@@ -330,6 +367,9 @@ $(document).ready(function() {
 
                 if (darthSecond) {
                     enemyScore = rey.healthPoints;
+                    enemyAttackPower = rey.counterAttackPower;
+                    enemyName = rey.fullName;
+
                     $('.bottomRight').animate({
                             opacity: '1'
                         },
@@ -341,6 +381,9 @@ $(document).ready(function() {
                         });
                 } else {
                     enemyScore = dart.healthPoints;
+                    enemyAttackPower = dart.counterAttackPower;
+                    enemyName = dart.fullName;
+
                     $('.bottomRight').animate({
                             opacity: '1'
                         },
@@ -351,13 +394,127 @@ $(document).ready(function() {
                             $('#scoreR').text(enemyScore);
                         });
                 }
-
             }
         }
+
+        // create button attack
+        $('.buttonGameStart').animate({
+                opacity: '1'
+            },
+            "slow", 'swing',
+            function() {
+                $('.buttonGameStart').html('<button type="submit" value="Attack">Attack</button>');
+            });
         start = false;
     });
 
+    // start game
+    $('.buttonGameStart').click(function() {
+        sound.play(); // play sound
+
+        $('#userCount').text("You attacked " + enemyName + " for " + userAttackPower + " damaged");
+        $('#enemyCount').text(enemyName + " attacked you back for " + enemyAttackPower + " damaged");
+
+        userCount -= enemyAttackPower;
+        enemyScore -= userAttackPower;
+
+        $('#scoreL').text(userCount);
+        $('#scoreR').text(enemyScore);
+
+        // change enemy character
+        if (enemyScore <= 0) {
+
+            userCount += 60;
+            count++;
+
+            $('.bottomRight').animate({
+                    opacity: '1'
+                },
+                "slow", 'swing',
+                function() {
+                    $('#nameR').html("");
+                    $('#newR').html("");
+                    $('#scoreR').text("");
+                    $('.buttonGameStart').html("");
+                });
+
+            $('#comment').text("You have defeated " + enemyName + " you can choose to fight another enemy from right");
+            $('#userCount').text("");
+            $('#enemyCount').text("");
+
+            userAttackPower += enemyAttackPower;
+
+            start = true;
+        }
 
 
+        // lose
+        if (userCount <= 0) {
+            sound.pause();
 
+            $('.buttonGameStart').animate({
+                    opacity: '1'
+                },
+                "slow", 'swing',
+                function() {
+                    $('.buttonGameStart').html("");
+                    $('#comment').text("You've been defeated press Play Again button for play again");
+                    $('#userCount').text("");
+                    $('#gameOver').text("GAME OVER");
+                    $('#enemyCount').hide();
+                    $('.bottomLeft').hide();
+                    $('.bottomMiddle').hide();
+                    $('.bottomRight').hide();
+                    $('.top2').hide();
+                });
+
+            $('.buttonGameOver').animate({
+                    opacity: '1'
+                },
+                "slow", 'swing',
+                function() {
+                    $('.buttonGameOver').html('<button type="submit" value="Attack">Play Again</button>');
+                });
+
+            $('.buttonGameOver').click(function() {
+                window.location = "index.html"
+            });
+
+        }
+
+        // win
+        if (count === 3) {
+            userUpper = userName.toUpperCase();
+            sound.pause();
+
+            $('.buttonGameStart').animate({
+                    opacity: '1'
+                },
+                "slow", 'swing',
+                function() {
+                    $('.buttonGameStart').html("");
+                    $('#comment').text('Press "Play Again" button for play again');
+                    $('#userCount').text("");
+                    $('#gameOver').text(userUpper + " IS WIN");
+                    $('#winner').html(winnerImage);
+
+                    $('#enemyCount').hide();
+                    $('.bottomLeft').hide();
+                    $('.bottomMiddle').hide();
+                    $('.bottomRight').hide();
+                    $('.top2').hide();
+                });
+
+            $('.buttonGameOver').animate({
+                    opacity: '1'
+                },
+                "slow", 'swing',
+                function() {
+                    $('.buttonGameOver').html('<button type="submit" value="Attack">Play Again</button>');
+                });
+            $('.buttonGameOver').click(function() {
+                window.location = "index.html"
+            });
+        }
+    });
 });
